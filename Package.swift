ae494545acf1e78f48e3233057ca105072eb85bb
Hedi MESTIRI <mestiri.hedi@gmail.com>
2020-11-17 08:31:24 +0100

    Remove unused framework

---
+++ Package.swift
@@ -0,0 +1,70 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "AWSSDK",
+    defaultLocalization: "en",
+    platforms: [.iOS(.v12)],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(name: "AWSCore", targets: ["AWSCore"]),
+        .library(name: "AWSIoT", targets: ["AWSIoT"]),
+        .library(name: "AWSCognitoIdentityProviderASF", targets: ["AWSCognitoIdentityProviderASF"]),
+        .library(name: "AWSCognitoIdentityProvider", targets: ["AWSCognitoIdentityProvider"]),
+        .library(name: "AWSCognitoAuth", targets: ["AWSCognitoAuth"]),
+        .library(name: "AWSCognito", targets: ["AWSCognito"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(name: "AWSCore",
+                path: "AWSCore",
+                cSettings: [
+                    .headerSearchPath("./Authentication"),
+                    .headerSearchPath("./Utility"),
+                    .headerSearchPath("./Serialization"),
+                    .headerSearchPath("./Service"),
+                    .headerSearchPath("./Networking"),
+                    .headerSearchPath("./Mantle"),
+                    .headerSearchPath("./Mantle/extobjc"),
+                    .headerSearchPath("./Logging"),
+                    .headerSearchPath("./Logging/Extensions"),
+                    .headerSearchPath("./Fabric"),
+                    .headerSearchPath("./GZIP"),
+                    .headerSearchPath("./Bolts"),
+                    .headerSearchPath("./XMLWriter"),
+                    .headerSearchPath("./XMLDictionary"),
+                    .headerSearchPath("./FMDB"),
+                    .headerSearchPath("./CognitoIdentity"),
+                    .headerSearchPath("./KSReachability"),
+                    .headerSearchPath("./TMCache"),
+                    .headerSearchPath("./UICKeyChainStore"),
+                    .headerSearchPath("./STS"),
+                    .headerSearchPath("./"),
+                    .headerSearchPath("")
+                ]),
+        .target(name: "AWSIoT",
+                dependencies: ["AWSCore"],
+                path: "AWSIoT",
+                cSettings: [
+                    .headerSearchPath(""),
+                    .headerSearchPath("./Internal"),
+                    .headerSearchPath("./Internal/MQTTSDK"),
+                    .headerSearchPath("./Internal/SocketRocket"),
+                    .headerSearchPath("../AWSCore/Logging"),
+                    .headerSearchPath("../AWSCore/Networking"),
+                ]),
+        .target(name: "AWSCognitoIdentityProviderASF",
+                path: "AWSCognitoIdentityProviderASF"),
+        .target(name: "AWSCognitoIdentityProvider",
+                dependencies: ["AWSCore", "AWSCognitoIdentityProviderASF"],
+                path: "AWSCognitoIdentityProvider"),
+        .target(name: "AWSCognitoAuth",
+                dependencies: ["AWSCore", "AWSCognitoIdentityProviderASF"],
+                path: "AWSCognitoAuth"),
+        .target(name: "AWSCognito",
+                dependencies: ["AWSCore"],
+                path: "AWSCognito")
+    ]
+)
